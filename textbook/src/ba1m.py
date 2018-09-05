1413c4e6fd3484892a4470f51e73999f46e29972
dibaggioj <dibaggioj@gmail.com>
2018-09-04 20:46:22 -0600

    Solve ba1m: Implement NumberToPattern

--- textbook/src/ba1m.py
+++ textbook/src/ba1m.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env
 # encoding: utf-8
 """
-Created by John DiBaggio on 2018-08-10
+Created by John DiBaggio on 2018-09-04
 
 Implement NumberToPattern
 
@@ -17,8 +17,8 @@ Sample Dataset
 4
 Sample Output
 AGTC
-Extra Dataset
 
+Extra Dataset
 Input
 5353
 7
@@ -41,53 +41,22 @@ else:
 
 argv = list(sys.argv)
 
-input_text = ""
-input_k = 0
-input_mismatches = 0
+input_number = -1
+input_k = -1
 
 for line in fileinput.input(argv[1]):
     if len(line) > 0:
-        if " " in line:
-            vals = line.split(" ")
-            if len(vals) == 2:
-                input_k = int(vals[0])
-                input_mismatches = int(vals[1])
-        else:
-            input_text += line.replace('\n', '')
+        if input_number == -1:
+            input_number = int(line.replace('\n', ''))
+        elif input_k == -1:
+            input_k = int(line.replace('\n', ''))
 
 
-def find_frequent_words_with_mismatches(text, k, d):
-    buffer = text[0:k]
-    text = text[k:]
-    words = dict()
-    for n in BioUtil.neighbors(buffer, d):
-        words[n] = 1
-    for c in text:
-        buffer = buffer[1:k] + c
-        for n in BioUtil.neighbors(buffer, d):
-            if n in words:
-                words[n] += 1
-            else:
-                words[n] = 1
-    most_frequent_words = list()
-    max_frequency = 0
-    for word, frequency in words.items():
-        if frequency > max_frequency:
-            max_frequency = frequency
-            most_frequent_words = list([word])
-        elif frequency == max_frequency:
-            most_frequent_words.append(word)
+output_string = BioUtil.number_to_pattern(input_number, input_k)
 
-    return most_frequent_words
-
-
-frequent_words = find_frequent_words_with_mismatches(input_text, input_k, input_mismatches)
-
-output_string = " ".join(str(i) for i in frequent_words)
-
-print("The most frequent {}-mers with {} mismatches in \"{}\" are:\n{}".format(input_k, input_mismatches, input_text,
-                                                                               output_string))
+print("The {}-mer pattern for number {} is \"{}\"".format(str(input_k), str(input_number), output_string))
 
 output_file = open(argv[2], "w+")
 output_file.write(output_string)
 output_file.close()
+
